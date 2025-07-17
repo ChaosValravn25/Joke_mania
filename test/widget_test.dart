17483ff1e89aff20d496447db8b385934964136f
ChaosValravn25 <silver.wolf1@hotmail.com>
2025-07-17 19:58:09 -0400

    cambios y arreglos en pubspec.lock
    un pequeño error que no conocía el main.dart, pero se corrigió

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Joke_mania/main.dart';
+import 'package:joke_mania/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
